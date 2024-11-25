9cd198b129566b58e41cfb67076b06565be5fb69
nguyenam5122004 <nguyennam5122004@gmail.com>
2024-11-25 18:53:49 +0700

    Bai 3

---
+++ Controllers/StudentController.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+
+namespace Project1.Controllers
+{
+    public class StudentController : Controller
+    {
+        private static List<Student> Students = new List<Student>();
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Register(Student model)
+        {
+            if (ModelState.IsValid)
+            {
+                Students.Add(model);
+
+                TempData["MSSV"] = model.MSSV;
+                TempData["HoTen"] = model.HoTen;
+                TempData["ChuyenNganh"] = model.ChuyenNganh;
+
+                TempData["SoLuongCungNganh"] = Students.FindAll(s => s.ChuyenNganh == model.ChuyenNganh).Count;
+
+                return RedirectToAction("ShowKQ");
+            }
+
+            return View("Index");
+        }
+        public IActionResult ShowKQ()
+        {
+            return View();
+        }
+    }
+
+    public class Student
+    {
+        public string MSSV { get; set; }
+        public string HoTen { get; set; }
+        public double DiemTB { get; set; }
+        public string ChuyenNganh { get; set; }
+    }
+}
